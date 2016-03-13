cc8345cc31c9252de9b6c5e247503d92e1229634
Bernard Spil <spil.oss@gmail.com>
2016-03-13 21:53:28 +0100

    Add additional variables to config.sh
      - Donation setting
      - Inverter S/N from Goodwe
      - Structure settings in groups
      - Add more comments for settings

--- Goodwe2PVouput/config.sh
+++ Goodwe2PVouput/config.sh
@@ -1,9 +1,26 @@
-User specific settings
+# User specific settings
 
+
+### Goodwe settings
+# Can be found in the URL when you're on one of the detail pages
 stationId="<your Goodwe-power.com stationID>"
+# Inverter S/N
+inverterSN="<The inverter's serial number>"
+
+### PVOutput settings
+# Find or generate "API Key" in "Settings"
 apiKey="<your PVoutput.org API key>"
+# System ID in "Registered Systems"
 sysId="<This system's PVoutput.org system number"
+# Set to "yes" if you've donated (higher rate-limit, more history)
+donation=
+
+### Your PV installation
+# Where the panels are installed (for sunrise/sunset calculation)
 latitude=<latitude of your installation>
 longitude=-<longitude of your installation>
+
+### Settings for this machine
+# Where you want to store the output CSV files
 outputPath="<Where to store the csv>"
 
